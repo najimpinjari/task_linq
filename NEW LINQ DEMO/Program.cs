92b60bb76662a3935a915d769061d1c9a9a23af7
NAJIMdp41 <143899549+najimpinjari@users.noreply.github.com>
2023-12-26 18:28:11 +0530

    Add files via upload

---
+++ NEW LINQ DEMO/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NEW_LINQ_DEMO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<int> numbers1 = Enumerable.Empty<int>();
+            Console.WriteLine($"{numbers1.Count()}");
+
+            IEnumerable<int> numbers2 = Enumerable.DefaultIfEmpty<int>(numbers1);
+            Console.WriteLine($"{numbers2.Count()} : {numbers2.ElementAt(0)}");
+
+            IEnumerable<int> numbers3 = Enumerable.Repeat(5, 10);
+            foreach (var item in numbers3)
+            {
+                Console.Write($"{item} ");
+            }
+            Console.WriteLine();
+
+            IEnumerable<int> numbers4 = Enumerable.Range(1, 25);
+            foreach (var item in numbers4)
+            {
+                Console.Write($"{item} ");
+            }
+            Console.WriteLine();
+
+            Console.ReadLine();
+        }
+    }
+}
