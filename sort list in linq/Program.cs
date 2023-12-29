7ff4c8f300549c86aba6a2c294c64ba6b4b22ffa
NAJIMdp41 <143899549+najimpinjari@users.noreply.github.com>
2023-12-29 12:54:48 +0530

    Add files via upload

---
+++ sort list in linq/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sort_list_in_linq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = new List<string> {"najim", "arbaz" , "rupesh" ,"nilesh", "shailesh" };
+
+
+            var sortnum = names.OrderBy(x => x, StringComparer.OrdinalIgnoreCase);
+
+            foreach (string name in sortnum)
+            {
+                Console.WriteLine(name);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
