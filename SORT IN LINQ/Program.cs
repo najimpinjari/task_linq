a6635b2939ce8d0537e680b0dea0350c18558946
NAJIMdp41 <143899549+najimpinjari@users.noreply.github.com>
2023-12-29 12:55:11 +0530

    Add files via upload

---
+++ SORT IN LINQ/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SORT_IN_LINQ
+{
+    class Program
+    {
+        static void Main()
+        {   
+            //sample data list of intiger values 
+            List<int> numbers  = new List<int> {9,2,3,7,4,8,1};
+
+            //sorting in asending and disending order 
+            var sortnum = numbers.OrderByDescending( n => n); 
+
+
+            //for displaing sorting numbers
+            foreach (var number in sortnum)
+            {
+                Console.WriteLine(number);
+            }
+            Console.ReadLine();
+        }
+    }
+}
