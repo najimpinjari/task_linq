51f7822ab860da960ffeef059ee200693891bc8a
NAJIMdp41 <143899549+najimpinjari@users.noreply.github.com>
2024-01-02 18:34:35 +0530

    Add files via upload

---
+++ Element opaatos  for linq/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Element_opaatos__for_linq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Element_opaatos__for_linq
+
+            #region first 
+            List<int> num = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, };
+            int firsteven = num.First(x => x % 2 == 0);
+
+            Console.WriteLine(firsteven);
+            Console.ReadLine();
+
+            #endregion first 
+
+
+            #region FirstOrDefault 
+            List<int> num = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, };
+            int firstordefault = num.FirstOrDefault(x => x % 2 == 0);  //1
+            Console.WriteLine(firstordefault);
+
+
+            List<int> numbers = new List<int> { 1, 3, 5 }; // 0
+            int firstEvenOrDefault = numbers.FirstOrDefault(x => x % 2 == 0);
+            Console.WriteLine($"First even number or default: {firstEvenOrDefault}");
+
+            Console.ReadLine();
+
+            #endregion FirstOrDefault 
+
+
+            #region last 
+            List<int> num = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, };
+            //  int lastOdd = num.Last(x => x % 2 != 0);
+            int lastOdd = num.Last();
+
+
+            Console.WriteLine(lastOdd);
+            Console.ReadLine();
+
+            #endregion last 
+
+
+
+            #region LastOrDefault 
+
+            List<int> num = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, };
+            int lastOddOrDefault = num.LastOrDefault(x => x % 2 != 0);
+
+            Console.WriteLine(lastOddOrDefault);
+
+            Console.ReadLine();
+
+
+            #endregion LastOrDefault
+
+        }
+    }
+}
---
+++ Element opaatos  for linq/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Element opaatos  for linq")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Element opaatos  for linq")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2ca5ae5b-5813-4910-b105-6e77a35d8e77")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
