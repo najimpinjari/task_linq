4a6f35ca439ead706d4f6fc810a3738f097fabe3
NAJIMdp41 <143899549+najimpinjari@users.noreply.github.com>
2024-01-04 17:23:29 +0530

    Add files via upload

---
+++ JOIN OPRETORS/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JOIN_OPRETORS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<TRAINER> trainers = TRAINER.GetTrainer(); // for trainers info call get get trainer list or method so you can hve a list of traners
+
+            Console.WriteLine("*** all student ***");
+            foreach (var item in trainers)
+            {
+                Console.WriteLine($" trainerId : {item.TrainerId} trainername : {item.TrainerName} experiance : {item.Experiance} ");
+
+            }
+            //all student for collection 
+
+            List<Student> students = Student.GetStudent();
+            Console.WriteLine("*** all students ***");
+
+            foreach(var item in students)
+            {
+                Console.WriteLine($" rollnumber : {item.Rollnumber} studentname : {item.StudentName} city : {item.city} trainerId : {item.TrainerId}");
+            }
+
+            //method syntax 
+
+            var result =
+                students.Join(trainers, s => s.TrainerId, t => t.TrainerId, (s, t)
+                => new { studentname = s.StudentName, Trainername = t.TrainerName });
+            Console.WriteLine("*** student  join trainers ***");
+
+            foreach(var item in result)
+            {
+                Console.WriteLine($" Student name : {item.studentname} trainer name : {item.Trainername}"); 
+            }
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ JOIN OPRETORS/TRAINER.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JOIN_OPRETORS
+{
+    public class TRAINER
+    {
+        public int TrainerId { get; set; }
+                                                // trainer class property 
+        public string TrainerName { get; set; }
+        public int Experiance {  get; set; }
+
+        public static List<TRAINER> GetTrainer() //method 
+        {
+            return new List<TRAINER>()
+            {
+                new TRAINER () { TrainerId = 1, TrainerName = "vikul" ,Experiance = 14},
+                new TRAINER () { TrainerId = 2, TrainerName = "usha" ,Experiance = 2},
+                new TRAINER () { TrainerId = 3, TrainerName = "atul" ,Experiance = 3 }
+
+            };
+        }
+
+    }
+    public class Student
+    {
+        public int Rollnumber { get; set; }
+        public string StudentName { get; set;}      // student class property
+        public string city { get; set;}
+
+        public int? TrainerId { get; set;}
+
+        public static List<Student> GetStudent()
+        {
+            return new List<Student>()
+            {
+                new Student () {Rollnumber = 1,StudentName ="jayesh", city = "dhule", TrainerId = 1 },
+                new Student () {Rollnumber = 2,StudentName ="rahesh", city = "nasik", TrainerId = 2 },
+                new Student () {Rollnumber = 3,StudentName ="prathm", city = "satara", TrainerId = 1 },
+                new Student () {Rollnumber = 4,StudentName ="sham", city = "jalgoan", TrainerId = null },
+                new Student () {Rollnumber = 5,StudentName ="najim", city = "sakri", TrainerId = null },
+
+
+
+            };
+
+        }
+
+
+    }
+
+   
+}
